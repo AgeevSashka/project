15182bc41690a1573de8c2bee5b79d165c9472ae
Alex <kingno04061994@gmail.com>
2020-05-12 17:45:48 +0300

    Сделано третье задание

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    hisOc = +prompt("На сколько оцените его?", ""),
+    anyLastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+    anyHisOc = +prompt("На сколько оцените его?", "");
+
+console.log(`Первый фильм ${lastFilm} и его оценка ${hisOc}, второй фильм ${anyLastFilm} и его оценка ${anyHisOc}`);
